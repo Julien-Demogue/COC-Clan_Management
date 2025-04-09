3a7984a0d368c5cb0b567cc89d5021c6076dfbff
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2025-04-09 11:06:36 +0200

    Improve frontend api calls

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -1,40 +1,46 @@
+import type { ApiResponse } from '@/entities/ApiResponse.interface';
 import axios from 'axios';
 
-function sendApiError(message: string) {
-    return { error: true, message };
+export async function fetchClanInfo(clanTag: string) : Promise<ApiResponse<any>> {
+    return await fetchClan<any>(`http://localhost:3000/api/clans/%23${encodeURIComponent(clanTag)}`);
 }
 
-export async function fetchClanInfo(clanTag: string) {
-    if (!clanTag) {
-        return sendApiError('Clan tag is required');
-    }
+export async function fetchClanMembers(clanTag: string) : Promise<ApiResponse<any>> {
+    return await fetchClan<any>(`http://localhost:3000/api/clans/%23${encodeURIComponent(clanTag)}/members`);
+}
 
+/*
+    * Sends an error response for the API.
+    * @param error The error message to include in the response.
+    * @returns An ApiResponse object with success set to false and the provided error message.
+*/
+function sendApiError<T>(error: string) : ApiResponse<T> {
+    return { success: false, error };
+}
+
+/*
+    * Sends a successful API response.
+    * @param data The data to include in the response.
+    * @returns An ApiResponse object with success set to true and the provided data.
+*/
+function sendApiSuccess<T>(data: T) : ApiResponse<T> {
+    return { success: true, data };
+}
+
+/*
+    * Fetches clan information from the API.
+    * @param url The URL to fetch data from.
+    * @returns A promise that resolves to an ApiResponse object containing the clan data or an error message.
+*/
+async function fetchClan<T>(url: string) : Promise<ApiResponse<T>> { 
     try {
-        const response = await axios.get(`http://localhost:3000/api/clans/%23${encodeURIComponent(clanTag)}`);
-        console.log(response.data);
+        const response = await axios.get(url);
+        return sendApiSuccess(response.data);
     } catch (error: any) {
         if (error.response && error.response.status === 404) {
             return sendApiError('Clan not found');
         } else {
             console.error('Error fetching clan info:', error.message);
-            return sendApiError('An unexpected error occurred');
-        }
-    }
-}
-
-export async function fetchClanMembers(clanTag: string) {
-    if (!clanTag) {
-        return sendApiError('Clan tag is required');
-    }
-
-    try {
-        const response = await axios.get(`http://localhost:3000/api/clans/%23${encodeURIComponent(clanTag)}/members`);
-        console.log(response.data);
-    } catch (error: any) {
-        if (error.response && error.response.status === 404) {
-            return sendApiError('Clan not found');
-        } else {
-            console.error('Error fetching clan members:', error.message);
             return sendApiError('An unexpected error occurred');
         }
     }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,23 +1,61 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { fetchClanInfo, fetchClanMembers } from '@/services/apiService';
-
-const clanTag = ref('');
-</script>
-
 <template>
   <div class="flex flex-col items-center justify-center h-screen bg-background">
     <h1 class="mb-12 text-6xl font-bold text-secondary">Your clan tag</h1>
     <div class="flex items-center w-1/2 h-16 rounded-full bg-secondary">
       <span class="px-8 text-primary text-2xl font-bold">#</span>
-      <input type="text" v-model="clanTag"
+      <input type="text" v-model="clanTag" @keyup.enter="onValidateInput"
         class="flex-1 h-full bg-transparent text-primary text-left font-semibold text-xl outline-none" />
     </div>
-    <p class="mt-4 text-red font-semibold text-xl">error message</p>
-    <button
-      class="mt-12 px-8 py-2 text-xl font-bold text-primary bg-secondary rounded-full hover:bg-primary hover:text-secondary transition duration-300 ease-in-out"
-      v-on:click="fetchClanMembers(clanTag)">
-      Test api
-    </button>
+    <p class="mt-4 text-red font-semibold text-xl">{{ errorMessage }}</p>
   </div>
 </template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { fetchClanInfo, fetchClanMembers } from '@/services/apiService';
+
+const clanTag = ref('');
+const errorMessage = ref('');
+
+async function getClanInfo(clanTag: string): Promise<boolean> {
+  const result = await fetchClanInfo(clanTag);
+
+  if (!result.success) {
+    errorMessage.value = result.error || 'Unknown error';
+    return false;
+  }
+
+  errorMessage.value = '';
+  const clanInfo = result.data;
+  console.log('Clan Info:', clanInfo);
+  return true;
+}
+
+async function getClanMembers(clanTag: string): Promise<boolean> {
+  const result = await fetchClanMembers(clanTag);
+
+  if (!result.success) {
+    errorMessage.value = result.error || 'Unknown error';
+    return false;
+  }
+
+  errorMessage.value = '';
+  const clanMembers = result.data;
+  console.log('Clan Members:', clanMembers);
+  return true;
+}
+
+async function onValidateInput() {
+  if (!clanTag || clanTag.value.trim() === '') {
+    errorMessage.value = 'Clan tag is required';
+    return;
+  }
+
+  const isFetchSuccessful = await getClanInfo(clanTag.value);
+
+  if (isFetchSuccessful) {
+    await getClanMembers(clanTag.value);
+  }
+}
+</script>
