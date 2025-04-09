07d98d5c0cdf66720bbd0f7e1a660fc8069137dd
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2025-04-09 15:12:20 +0200

    Start dashboard page

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,9 +1,29 @@
 <template>
-    <Navbar />
-    <WIP />
+    <div class="flex flex-col h-screen">
+        <Navbar />
+        <div class="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-16">
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Winrate</p>
+            </div>
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Active members</p>
+            </div>
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Last MVP</p>
+            </div>
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Role distribution</p>
+            </div>
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Capital</p>
+            </div>
+            <div class="bg-primary text-secondary rounded-lg shadow-md flex items-top justify-center p-4">
+                <p class="text-2xl font-bold">Donations</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
-import WIP from '@/components/WIP.vue';
 </script>
